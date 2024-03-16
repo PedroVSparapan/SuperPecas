a81d663e624186dadc7eb19d64ecda7cfc2d2cb5
PedroVSparapan <163478974+PedroVSparapan@users.noreply.github.com>
2024-03-16 13:28:44 -0300

    Add files via upload

---
+++ superpecas/src/main/java/br/com/masterclass/superpecas/controller/CarroController.java
@@ -0,0 +1,19 @@
+package br.com.masterclass.superpecas.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/carro")
+
+public class CarroController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+
+    public String buscaCarro() {
+
+        return "teste";
+
+    }
+}
